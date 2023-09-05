bd22f4c9f225ccc4510e91e41d9bb0ab242438ad
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-09-05 13:14:26 +0700

    Create conf.sh: init

---
+++ conf.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+echo "NAT VPS Auto DNS Configure"
+echo "============================"
+echo ""
+echo "Disable resolved dns"
+systemctl stop systemd-resolved.service
+systemctl disable systemd-resolved.service
+echo ""
+echo "using plain dns resolv"
+rm /etc/resolv.conf
+rm /etc/resolvconf -rf
+rm /etc/network/*/resolved
+cat > /etc/resolv.conf <<-END
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+nameserver 2001:4860:4860::8888
+nameserver 2001:4860:4860::8844
+END
+chattr +i /etc/resolv.conf
+echo ""
+echo "Restarting network service..."
+service networking restart
+service network status
+echo "DONE 👍"
